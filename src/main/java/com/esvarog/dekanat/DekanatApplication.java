1440ad2b036650ca55dac0fa1a5bc9dfc4d6e8d4
Svarog <poulpotter1@gmail.com>
2023-12-06 14:33:37 +0200

    import3.0

--- src/main/java/com/esvarog/dekanat/DekanatApplication.java
+++ src/main/java/com/esvarog/dekanat/DekanatApplication.java
@@ -13,32 +13,18 @@ import java.io.File;
 
 @SpringBootApplication
 public class DekanatApplication {
-    private final DataInitializerAssessment dataInitializerAssessment;
-    private final DataInitializerDepartment dataInitializerDepartment;
-    private final DataInitializerEduDoc dataInitializerEduDoc;
-    private final DataInitializerFaculty dataInitializerFaculty;
-    private final DataInitializerGroup dataInitializerGroup;
-    private final DataInitializerOrder dataInitializerOrder;
-    private final DataInitializerRegion dataInitializerRegion;
     private final DataInitializerUser dataInitializerUser;
 
 
-    public DekanatApplication(DataInitializerAssessment dataInitializerAssessment, DataInitializerDepartment dataInitializerDepartment, DataInitializerEduDoc dataInitializerEduDoc, DataInitializerFaculty dataInitializerFaculty, DataInitializerGroup dataInitializerGroup, DataInitializerOrder dataInitializerOrder, DataInitializerRegion dataInitializerRegion, DataInitializerUser dataInitializerUser) {
-        this.dataInitializerAssessment = dataInitializerAssessment;
-        this.dataInitializerDepartment = dataInitializerDepartment;
-        this.dataInitializerEduDoc = dataInitializerEduDoc;
-        this.dataInitializerFaculty = dataInitializerFaculty;
-        this.dataInitializerGroup = dataInitializerGroup;
-        this.dataInitializerOrder = dataInitializerOrder;
-        this.dataInitializerRegion = dataInitializerRegion;
+    public DekanatApplication(DataInitializerUser dataInitializerUser) {
+
         this.dataInitializerUser = dataInitializerUser;
     }
 
     public static void main(String[] args) throws TelegramApiException {
         Dotenv dotenv = Dotenv.load();
-        String botToken = dotenv.get("TELEGRAM_BOT_TOKEN");
+        String botToken = dotenv.get("TELEGRAM_BOT_TOKEN_DEV");
         String chatId = dotenv.get("TELEGRAM_CHAT_ID");
-        String temp = "";
 
         File logFile = new File("bot.log");
         if (logFile.exists()) {
@@ -63,13 +49,6 @@ public class DekanatApplication {
 
     @PostConstruct
     public void init() {
-        this.dataInitializerAssessment.init();
-        this.dataInitializerDepartment.init();
-        this.dataInitializerEduDoc.init();
-        this.dataInitializerFaculty.init();
-        this.dataInitializerGroup.init();
-        this.dataInitializerOrder.init();
-        this.dataInitializerRegion.init();
         this.dataInitializerUser.init();
     }
 }
--- src/main/java/com/esvarog/dekanat/security/SecurityConfiguration.java
+++ src/main/java/com/esvarog/dekanat/security/SecurityConfiguration.java
@@ -1,9 +1,5 @@
 package com.esvarog.dekanat.security;
 
-import com.esvarog.dekanat.security.JwtProvider;
-import com.esvarog.dekanat.security.initializer.JwtAuthenticationFilter;
-import com.esvarog.dekanat.service.JwtService;
-import com.esvarog.dekanat.service.UserService;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -12,7 +8,6 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,7 +24,6 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
